0da1f99547b2c8703a9f8eb1c3820a3fbe47d92c
Ishaan pandey <ishaan.pandey@mail.utoronto.ca>
2024-07-05 00:49:14 -0400

    Made getter and setter method for message

--- src/entity/Requests.java
+++ src/entity/Requests.java
@@ -1,6 +1,28 @@
 package entity;
 
 public class Requests {
-    private  User From;
-    private  User To;
+    private User From;
+    private User To;
+
+    public Requests(User From, User To) {
+        this.From = From;
+        this.To = To;
+    }
+
+    public User getFrom() {
+        return From;
+    }
+
+    public User getTo() {
+        return To;
+    }
+
+    public void setFrom(User From) {
+        this.From = From;
+    }
+
+    public void setTo(User To) {
+        this.To = To;
+    }
+
 }
