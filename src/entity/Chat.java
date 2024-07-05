4e3ee257f2faa4fb083cefa471d3317e840b5152
umer <umer.farooqui135@gmail.com>
2024-07-05 00:36:55 -0400

    added constructor method and getter and setter methods for user,java

--- src/entity/Chat.java
+++ src/entity/Chat.java
@@ -6,4 +6,35 @@ public class Chat {
      private ArrayList<User> users = new ArrayList<>();
      private Integer noOfMembers;
      private ArrayList<Message> allmessages = new ArrayList<>();
+
+     Chat(ArrayList<User> users, Integer noOfMembers, ArrayList<Message> allmessages) {
+          this.users = users;
+          this.noOfMembers = noOfMembers;
+          this.allmessages = allmessages;
+     }
+
+     public ArrayList<User> getUsers() {
+          return users;
+     }
+
+     public void setUsers(ArrayList<User> users) {
+          this.users = users;
+     }
+
+     public Integer getNoOfMembers() {
+          return noOfMembers;
+     }
+
+     public void setNoOfMembers(Integer noOfMembers) {
+          this.noOfMembers = noOfMembers;
+     }
+
+     public ArrayList<Message> getAllmessages() {
+          return allmessages;
+     }
+
+     public void setAllmessages(ArrayList<Message> allmessages) {
+          this.allmessages = allmessages;
+     }
+
 }
