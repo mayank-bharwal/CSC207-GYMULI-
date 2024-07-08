0259f086d166fb79de97d04311195ae0c12f1b9a
Grant <143639424+granthamblin@users.noreply.github.com>
2024-07-08 18:10:51 -0400

    cleaned up signup ui

--- src/views/SignupView.java
+++ src/views/SignupView.java
@@ -21,8 +21,6 @@ public class SignupView extends JPanel implements PropertyChangeListener {
 
         setLayout(new BorderLayout());
         setPreferredSize(new Dimension(800, 600));
-
-        // Create the form panel
         JPanel formPanel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
@@ -62,9 +60,44 @@ public class SignupView extends JPanel implements PropertyChangeListener {
         gbc.anchor = GridBagConstraints.WEST;
         formPanel.add(passwordField, gbc);
 
+        JLabel programOfStudyLabel = new JLabel("Program(s) of Study:");
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        gbc.anchor = GridBagConstraints.EAST;
+        formPanel.add(programOfStudyLabel, gbc);
+
+        JTextField programOfStudyField = new JTextField(20);
+        gbc.gridx = 1;
+        gbc.gridy = 5;
+        gbc.anchor = GridBagConstraints.WEST;
+        formPanel.add(programOfStudyField, gbc);
+
+        JLabel interestsLabel = new JLabel("Interests:");
+        gbc.gridx = 0;
+        gbc.gridy = 7;
+        gbc.anchor = GridBagConstraints.EAST;
+        formPanel.add(interestsLabel, gbc);
+
+        JPanel interestsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
+        JTextField interest1Field = new JTextField(10);
+        JTextField interest2Field = new JTextField(10);
+        JTextField interest3Field = new JTextField(10);
+        interestsPanel.add(interest1Field);
+        interestsPanel.add(interest2Field);
+        interestsPanel.add(interest3Field);
+
+        gbc.gridx = 1;
+        gbc.gridy = 7;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.WEST;
+        formPanel.add(interestsPanel, gbc);
+
+        gbc.gridwidth = 1;
+
+
         JButton signupButton = new JButton("Signup");
         gbc.gridx = 0;
-        gbc.gridy = 4;
+        gbc.gridy = 9;
         gbc.gridwidth = 2;
         gbc.anchor = GridBagConstraints.CENTER;
         signupButton.addActionListener(e -> {
@@ -75,10 +108,11 @@ public class SignupView extends JPanel implements PropertyChangeListener {
 
         JButton loginButton = new JButton("Go to Login");
         loginButton.addActionListener(e -> viewModelManager.setActiveView("LoginView"));
-        gbc.gridy = 5;
+        gbc.gridy = 10;
         formPanel.add(loginButton, gbc);
 
         add(formPanel, BorderLayout.CENTER);
+
     }
 
     @Override
