5796faa932601a8088d4b46215d4e579df549922
Grant <143639424+granthamblin@users.noreply.github.com>
2024-07-08 14:38:21 -0400

    test ui

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,21 @@
 package app;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
+    public static void main(String[] args) {
+        // creates window
+        JFrame frame = new JFrame("java swing test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+
+        JButton button = new JButton("click");
+
+        // adds button to pane
+        frame.getContentPane().add(button);
+
+        frame.setVisible(true);
+    }
 }
