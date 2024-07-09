d0096b5ac125385d2dac888b0bfabc3be15aeede
Grant <143639424+granthamblin@users.noreply.github.com>
2024-07-08 21:21:30 -0400

    implemented majority of use case

--- src/use_case/account_creation/AccountCreationOutputData.java
+++ src/use_case/account_creation/AccountCreationOutputData.java
@@ -2,11 +2,17 @@ package use_case.account_creation;
 
 public class AccountCreationOutputData {
     private final String username;
+    private String creationTime;
+    private boolean failView;
 
-    public AccountCreationOutputData(String username) {
+    public AccountCreationOutputData(String username, String creationTime, boolean failView ) {
         this.username = username;
+        this.creationTime = creationTime;
+        this.failView = failView;
     }
     public String getUsername() {
         return username;
     }
+    public String getCreationTime() {return creationTime; }
+    public void setCreationTime(String creationTime) {this.creationTime = creationTime; }
 }
