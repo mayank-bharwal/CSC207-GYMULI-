6d79d4e3e8f95471d100444597e0a63f5548c1e1
Mayank Bharwal <119955673+mayank-bharwal@users.noreply.github.com>
2024-07-11 20:02:01 -0400

    Partially implemented the GroupCreation use case

--- src/entity/GroupFactory.java
+++ src/entity/GroupFactory.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GroupFactory {
+public interface GroupFactory {
     public Group createGroup(String groupName, ArrayList<User> users, String bio, String image,
                              LocalDateTime dateCreated){
         return new Group(groupName, users, bio, image, dateCreated);
