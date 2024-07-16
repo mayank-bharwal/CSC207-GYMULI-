169d6d7e715985ffc83f8f928b2fd5558b407597
haoyuech <yuechen.hao@mail.utoronto.ca>
2024-07-15 20:35:31 -0400

    javadoc for use cases

--- src/interface_adapter/edit_friends/EditFriendsController.java
+++ src/interface_adapter/edit_friends/EditFriendsController.java
@@ -2,6 +2,7 @@ package interface_adapter.edit_friends;
 
 import use_case.edit_friends.EditFriendsInputBoundary;
 import use_case.edit_friends.EditFriendsInputData;
+import entity.User;
 
 public class EditFriendsController {
     final EditFriendsInputBoundary editFriendsCaseInteractor;
@@ -10,7 +11,7 @@ public class EditFriendsController {
         this.editFriendsCaseInteractor = editFriendsCaseInteractor;
     }
 
-    public void execute(String user1, String user2, String user3){
+    public void execute(User user1, User user2, User user3){
         EditFriendsInputData editFriendsInputData = new EditFriendsInputData(user1, user2, user3);
 
         editFriendsCaseInteractor.execute(editFriendsInputData);
--- src/interface_adapter/send_message/SendMessageController.java
+++ src/interface_adapter/send_message/SendMessageController.java
@@ -1,4 +1,22 @@
 package interface_adapter.send_message;
 
+import use_case.send_message.SendMessageInputBoundary;
+import use_case.send_message.SendMessageInputData;
+import entity.Message;
+
+import java.time.LocalDateTime;
+
 public class SendMessageController {
+    final SendMessageInputBoundary sendMessageUseCaseInteractor;
+
+    public SendMessageController(SendMessageInputBoundary sendMessageCaseInteractor) {
+        this.sendMessageUseCaseInteractor = sendMessageUseCaseInteractor;
+    }
+
+    public void execute(String message, String sender, String receiver){
+        SendMessageInputData sendMessageInputData = new Message(chatName, sender, receiver, message, LocalDateTime.now());
+        sendMessageUseCaseInteractor.sendMessageInputData(message);
+
+        sendMessageUseCaseInteractor.execute(sendMessageInputData);
+    }
 }
