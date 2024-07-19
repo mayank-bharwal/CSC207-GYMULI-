f2159fea782ff8675b278f86522ad1130266c1b1
Muhammad Umer Farooqui <141590838+UmerFq56@users.noreply.github.com>
2024-07-18 20:17:56 -0400

    did some progress on IA

--- src/interface_adapter/update_profile/UpdateProfileViewModel.java
+++ src/interface_adapter/update_profile/UpdateProfileViewModel.java
@@ -1,4 +1,15 @@
 package interface_adapter.update_profile;
 
-public class UpdateProfileViewModel {
+import interface_adapter.ViewModel;
+
+public class UpdateProfileViewModel extends ViewModel {
+
+    public static final String CLEAR_BUTTON_LABEL = "Clear";
+    public static final String TITLE_LABEL = "Update Profile";
+    public static final String CURRENT_USERNAME_LABEL = "Enter current username";
+    public static final String CURRENT_PASSWORD_LABEL = "Enter current password";
+    public static final String USERNAME_LABEL = " Update Username";
+    public static final String PASSWORD_LABEL = " Update Password";
+    public static final String USERNAME_LABEL = " Update Username";
+
 }
