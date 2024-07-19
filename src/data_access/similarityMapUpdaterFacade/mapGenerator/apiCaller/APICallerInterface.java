2e299ca9a36d9d7b00125f1a348f08978080454f
Mayank Bharwal <119955673+mayank-bharwal@users.noreply.github.com>
2024-07-18 23:45:29 -0400

    Delete src/data_access/similarityMapUpdaterFacade directory
    RESOLVING CONFLICT

--- src/data_access/similarityMapUpdaterFacade/mapGenerator/apiCaller/APICallerInterface.java
+++
@@ -1,5 +0,0 @@
-package data_access.similarityMapUpdaterFacade.mapGenerator.apiCaller;
-
-public interface APICallerInterface {
-    Float getSimilarityScore(String text1, String text2);
-}
--- src/data_access/similarityMapUpdaterFacade/mapUpdater/MapUpdaterInterface.java
+++
@@ -1,8 +0,0 @@
-package data_access.similarityMapUpdaterFacade.mapUpdater;
-
-import data_access.similarityMapUpdaterFacade.mapGenerator.MapGeneratorInterface;
-import org.json.JSONObject;
-
-public interface MapUpdaterInterface {
-    public void updateMap(JSONObject map );
-}
