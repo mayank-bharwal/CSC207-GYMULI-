d0dcdb867ae1fefbd1b9324227d148c54d960a59
Ishaan pandey <Ishaan.pandey@mail.utoronto.ca>
2024-07-19 22:29:34 -0400

    chat presenter for the retrieve chat

--- src/use_case/retrieve_message/RetrieveMessageUserDataAccessInterface.java
+++
@@ -1,6 +0,0 @@
-package use_case.retrieve_message;
-
-public interface RetrieveMessageUserDataAccessInterface {
-    boolean AccountExists(String username);
-    String getMessage(String sender, String receiver);
-}
