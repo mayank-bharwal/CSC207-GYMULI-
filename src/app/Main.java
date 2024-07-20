61d99cfa9b551470b95c18cd70bfda2a63a1176a
Grant Hamblin <granthamblin@Grants-MacBook-Pro.local>
2024-07-19 23:59:14 -0400

    updated name

--- src/app/Main.java
+++ src/app/Main.java
@@ -27,7 +27,7 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
 
-        JFrame application = new JFrame("Login Example");
+        JFrame application = new JFrame("GYMULI");
         application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         application.setPreferredSize(new Dimension(800, 600));
 
