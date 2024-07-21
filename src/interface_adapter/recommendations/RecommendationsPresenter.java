62edc4fac195c6029c6a4bede254a6153f6cc05e
Mayank Bharwal <119955673+mayank-bharwal@users.noreply.github.com>
2024-07-21 19:11:27 -0400

    Recommendation Interface Adapters

--- src/interface_adapter/recommendations/RecommendationsPresenter.java
+++ src/interface_adapter/recommendations/RecommendationsPresenter.java
@@ -1,4 +1,27 @@
 package interface_adapter.recommendations;
 
-public class RecommendationsPresenter {
+import interface_adapter.ViewModelManager;
+import use_case.recommendations.RecommendationsOutputBoundary;
+import use_case.recommendations.RecommendationsOutputData;
+
+public class RecommendationsPresenter implements RecommendationsOutputBoundary {
+
+    private final RecommendationsViewModel recommendationsViewModel;
+    private final ViewModelManager viewModelManager;
+
+    public RecommendationsPresenter(ViewModelManager viewModelManager, RecommendationsViewModel recommendationsViewModel) {
+        this.recommendationsViewModel = recommendationsViewModel;
+        this.viewModelManager = viewModelManager;
+    }
+
+    @Override
+    public void showSuccessScreen(RecommendationsOutputData recommendationsOutputData){
+
+    }
+
+    @Override
+    public void showFailureScreen(String errorMessage){
+
+    }
+
 }
