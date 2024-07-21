5e91598f26b1ca0d90b94a10c84fffacc33f7f64
Mayank Bharwal <119955673+mayank-bharwal@users.noreply.github.com>
2024-07-21 17:08:03 -0400

    Completing the implementation of recommendations Use Case

--- src/use_case/recommendations/RecommendationsInputData.java
+++ src/use_case/recommendations/RecommendationsInputData.java
@@ -1,5 +1,19 @@
 package use_case.recommendations;
 
+import entity.User;
+
 public class RecommendationsInputData {
 
+    private int numberOfRecommendations;
+    private User user;
+
+    public RecommendationsInputData(User user, int numberOfRecommendations) {
+        this.user = user;
+        this.numberOfRecommendations = numberOfRecommendations;
+    }
+
+    public int getNumberOfRecommendations() {return numberOfRecommendations;}
+    public User getUser() {return user;}
+    public void setUser(User user) {this.user = user;}
+    public void setNumberOfRecommendations(int numberOfRecommendations) {this.numberOfRecommendations = numberOfRecommendations;}
 }
