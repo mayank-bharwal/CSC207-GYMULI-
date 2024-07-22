35428f70a499a5d5693c8abe82cf4ce165ca38b3
Mayank Bharwal <119955673+mayank-bharwal@users.noreply.github.com>
2024-07-21 22:09:11 -0400

    changes in inteface adapter

--- src/interface_adapter/recommendations/RecommendationsViewModel.java
+++ src/interface_adapter/recommendations/RecommendationsViewModel.java
@@ -3,10 +3,29 @@ package interface_adapter.recommendations;
 import interface_adapter.ViewModel;
 import interface_adapter.search_user.SearchUserState;
 
+import javax.swing.*;
+import java.beans.PropertyChangeSupport;
+
 public class RecommendationsViewModel extends ViewModel {
 
     public final String RECOMMENDATIONS_TITLE = "recommendations view";
     public final String RECOMMENDATIONS_DETAILS = "Find Friends";
 
-    public SearchUserState state = new SearchUserState();
+    public final String RECOMMENDATION_BUTTON_LABEL = "Find Friends";
+    public final String NO_OF_USERS_LABEL = "How Many Similar Users";
+
+    public final String GO_BACK_BUTTON_LABEL = "Go Back";
+
+    private RecommendationsState state = new RecommendationsState();
+    private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    public final JTextField noofuserInputField = new JTextField(5);
+
+    public final JButton findFriendsButton = new JButton(RECOMMENDATION_BUTTON_LABEL);
+    public final JButton goBackButton = new JButton(GO_BACK_BUTTON_LABEL);
+
+    public RecommendationsState getState() {return state;}
+
+    public void setState(RecommendationsState state) {this.state = state;}
+
 }
