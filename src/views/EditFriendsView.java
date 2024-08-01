a5f9275969b39571800fdd4cfda1267a3585d8c5
Ishaan pandey <Ishaan.pandey@mail.utoronto.ca>
2024-08-01 12:24:41 -0400

    Friends View

---
+++ src/views/EditFriendsView.java
@@ -0,0 +1,24 @@
+//package views;
+//
+//import interface_adapter.ViewModelManager;
+//import interface_adapter.delete_chat.DeleteChatController;
+//import interface_adapter.delete_chat.DeleteChatViewModel;
+//
+//import javax.swing.*;
+//import java.awt.*;
+//import java.beans.PropertyChangeEvent;
+//import java.beans.PropertyChangeListener;
+//
+//public class EditFriendsView extends JPanel implements PropertyChangeListener {
+//    public static final String viewName = "EditFriendsView";
+//
+//    private final DeleteChatViewModel viewModel;
+//    private final DeleteChatController deleteChatController;
+//    private final ViewModelManager viewModelManager;
+//
+//
+//    @Override
+//    public void propertyChange(PropertyChangeEvent evt) {
+//
+//    }
+//}
