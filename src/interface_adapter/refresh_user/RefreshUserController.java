3b4687b073c5a700a89e35c05e7961d271120529
Ishaan pandey <Ishaan.pandey@mail.utoronto.ca>
2024-08-04 18:28:21 -0400

    Controller finished not tested yet

--- src/interface_adapter/refresh_user/RefreshUserController.java
+++ src/interface_adapter/refresh_user/RefreshUserController.java
@@ -1,4 +1,22 @@
 package interface_adapter.refresh_user;
 
+import use_case.refresh_user.RefreshUserInputBoundary;
+import use_case.refresh_user.RefreshUserInputData;
+
 public class RefreshUserController {
+
+    final RefreshUserInputBoundary refreshUserInteractor;
+
+    public RefreshUserController(RefreshUserInputBoundary refreshUserInteractor) {
+        this.refreshUserInteractor = refreshUserInteractor;
+    }
+
+    public void refreshUser(String username) {
+
+        RefreshUserInputData refreshUserInputData = new RefreshUserInputData(username);
+
+        refreshUserInteractor.refreshUser(refreshUserInputData);
+    }
 }
+
+
