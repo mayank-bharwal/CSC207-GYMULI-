a8279cd8e55849b3ccc082ddbba4a35e546d7faa
Ishaan pandey <Ishaan.pandey@mail.utoronto.ca>
2024-08-04 18:38:16 -0400

    RefreshUserViewModel complete not tested yet

--- src/interface_adapter/refresh_user/RefreshUserViewModel.java
+++ src/interface_adapter/refresh_user/RefreshUserViewModel.java
@@ -1,4 +1,31 @@
 package interface_adapter.refresh_user;
 
-public class RefreshUserViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class RefreshUserViewModel extends ViewModel {
+    private RefreshUserState state = new RefreshUserState();
+    private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    public RefreshUserState getState() {
+        return state;
+    }
+
+    public void setState(RefreshUserState state) {
+        this.state = state;
+    }
+
+    public void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+        propertyChangeSupport.firePropertyChange(propertyName, oldValue, newValue);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
 }
