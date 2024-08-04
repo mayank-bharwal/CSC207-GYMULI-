ed9d696ec40a2ba8db1477f362b92e3fab7325ac
Muhammad Umer Farooqui <141590838+UmerFq56@users.noreply.github.com>
2024-08-03 21:46:06 -0400

    dao for delete chat

--- src/interface_adapter/search_user/SearchUserViewModel.java
+++ src/interface_adapter/search_user/SearchUserViewModel.java
@@ -4,7 +4,7 @@ import interface_adapter.ViewModel;
 
 import javax.swing.text.View;
 
-public class SearchUserViewModel extends ViewModel {
+public class                       SearchUserViewModel extends ViewModel {
 
     public final String SU_TITLE = "Search User View";
     public final String SU_USERNAME = "Search Username";
