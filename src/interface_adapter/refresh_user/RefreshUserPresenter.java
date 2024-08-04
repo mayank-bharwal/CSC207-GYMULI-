eb4f5568e74819fee9df72e4bacd059782c31a9f
Ishaan pandey <Ishaan.pandey@mail.utoronto.ca>
2024-08-04 18:45:57 -0400

    presenter updated for the use case.

--- src/interface_adapter/refresh_user/RefreshUserPresenter.java
+++ src/interface_adapter/refresh_user/RefreshUserPresenter.java
@@ -1,4 +1,35 @@
 package interface_adapter.refresh_user;
 
-public class RefreshUserPresenter {
+import interface_adapter.ViewModelManager;
+import use_case.refresh_user.RefreshUserOutputBoundary;
+import use_case.refresh_user.RefreshUserOutputData;
+
+public class RefreshUserPresenter implements RefreshUserOutputBoundary {
+
+    private final RefreshUserViewModel refreshUserViewModel;
+    private final ViewModelManager viewModelManager;
+
+
+    public RefreshUserPresenter(RefreshUserViewModel refreshUserViewModel, ViewModelManager viewModelManager) {
+        this.refreshUserViewModel = refreshUserViewModel;
+        this.viewModelManager = viewModelManager;
+    }
+
+
+    @Override
+    public void setPassView(RefreshUserOutputData user) {
+        RefreshUserState refreshUserState = refreshUserViewModel.getState();
+        refreshUserState.setUsername(user.getUpdatedUser().getUsername());
+        this.refreshUserViewModel.setState(refreshUserState);
+        refreshUserViewModel.firePropertyChange("userUpdated", null, user.getUpdatedUser());
+
+    }
+
+    @Override
+    public void setFailView(String error) {
+        RefreshUserState refreshUserState = refreshUserViewModel.getState();
+        refreshUserState.setUsernameError(error);
+        refreshUserViewModel.firePropertyChange("userError", null, error);
+
+    }
 }
