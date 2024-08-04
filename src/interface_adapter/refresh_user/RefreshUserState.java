f8fc865f0e782354ebbb060d5e0f2552ff1b4817
Ishaan pandey <Ishaan.pandey@mail.utoronto.ca>
2024-08-04 18:33:02 -0400

    RefreshUserState complete not tested

--- src/interface_adapter/refresh_user/RefreshUserState.java
+++ src/interface_adapter/refresh_user/RefreshUserState.java
@@ -1,4 +1,30 @@
 package interface_adapter.refresh_user;
 
+
 public class RefreshUserState {
+    private String username = "";
+    private String usernameError = "";
+
+    public RefreshUserState(RefreshUserState copy) {
+        username = copy.username;
+        usernameError = copy.usernameError;
+    }
+
+    public RefreshUserState() {}
+
+    public String getUsernameError() {
+        return usernameError;
+    }
+
+    public void setUsernameError(String usernameError) {
+        this.usernameError = usernameError;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
