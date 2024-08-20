a7aaf491dd83f2d6fcb3468b6bae5d639452acaf
Muhammad Umer Farooqui <141590838+UmerFq56@users.noreply.github.com>
2024-08-20 19:19:36 -0400

    javadoc

--- src/use_case/update_profile/UpdateProfileInputBoundary.java
+++ src/use_case/update_profile/UpdateProfileInputBoundary.java
@@ -1,6 +1,18 @@
 
 package use_case.update_profile;
-//input boundary
+
+/**
+ * Input boundary for the Update Profile use case.
+ * Defines the method to execute the update profile operation.
+ */
+
 public interface UpdateProfileInputBoundary {
+
+    /**
+     * Executes the update profile use case with the provided input data.
+     *
+     * @param updateProfileInputData The data required to update the user's profile.
+     */
+
     void execute(UpdateProfileInputData updateProfileInputData);
 }
