8f85a21dd2241cbd9a25b27cacb00596e018c0e5
Ishaan pandey <Ishaan.pandey@mail.utoronto.ca>
2024-08-20 19:05:30 -0400

    just some java doc

--- src/use_case/make_chat/MakeChatInputData.java
+++ src/use_case/make_chat/MakeChatInputData.java
@@ -2,12 +2,27 @@ package use_case.make_chat;
 
 import java.time.LocalDateTime;
 
+/**
+ * This class represents the input data required for creating a new chat session.
+ * It contains the necessary information such as the chat name, the users involved,
+ * and the time the chat was created.
+ */
 public class MakeChatInputData {
-     final private String User_1;
-     final private String User_2;
-     final private String chatName;
-     final private LocalDateTime time;
 
+    private final String User_1;
+    private final String User_2;
+    private final String chatName;
+    private final LocalDateTime time;
+
+    /**
+     * Constructs a new instance of {@code MakeChatInputData} with the specified chat name,
+     * users, and time of creation.
+     *
+     * @param chatName the name of the chat.
+     * @param user1    the username or ID of the first user involved in the chat.
+     * @param user2    the username or ID of the second user involved in the chat.
+     * @param time     the time at which the chat is created.
+     */
     public MakeChatInputData(String chatName, String user1, String user2, LocalDateTime time) {
         this.chatName = chatName;
         this.User_1 = user1;
@@ -15,18 +30,38 @@ public class MakeChatInputData {
         this.time = time;
     }
 
+    /**
+     * Returns the name of the chat.
+     *
+     * @return the chat name.
+     */
     public String getChatName() {
         return chatName;
     }
 
+    /**
+     * Returns the username or ID of the first user involved in the chat.
+     *
+     * @return the first user's username or ID.
+     */
     public String getUser_1() {
         return User_1;
     }
 
+    /**
+     * Returns the username or ID of the second user involved in the chat.
+     *
+     * @return the second user's username or ID.
+     */
     public String getUser_2() {
         return User_2;
     }
 
+    /**
+     * Returns the time at which the chat was created.
+     *
+     * @return the creation time of the chat.
+     */
     public LocalDateTime getTime() {
         return time;
     }
--- src/use_case/make_chat/MakeChatUserDataAccessInterface.java
+++ src/use_case/make_chat/MakeChatUserDataAccessInterface.java
@@ -2,9 +2,34 @@ package use_case.make_chat;
 
 import entity.Chat;
 
+/**
+ * Interface for accessing and manipulating user and chat data in the "Make Chat" use case.
+ * It provides methods to check the existence of chats and users, as well as to save a new chat.
+ */
 public interface MakeChatUserDataAccessInterface {
 
+    /**
+     * Checks if a chat with the specified name already exists.
+     *
+     * @param chatName the name of the chat to check.
+     * @return {@code true} if the chat exists, {@code false} otherwise.
+     */
     boolean ChatExists(String chatName);
+
+    /**
+     * Checks if a user with the specified username exists.
+     *
+     * @param username the username of the user to check.
+     * @return {@code true} if the user exists, {@code false} otherwise.
+     */
     boolean UserExists(String username);
-    void saveChat(String user_1,String user_2, Chat chat);
+
+    /**
+     * Saves the specified chat with the associated users.
+     *
+     * @param user_1 the username or ID of the first user.
+     * @param user_2 the username or ID of the second user.
+     * @param chat   the chat entity to be saved.
+     */
+    void saveChat(String user_1, String user_2, Chat chat);
 }
